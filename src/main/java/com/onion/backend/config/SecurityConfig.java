90ef85388e20ecbb97bd15f2aae550c3dbb33bb2
Fillsogood <jdd05273@naver.com>
2025-03-05 15:57:16 +0900

    Swagger 적용

--- src/main/java/com/onion/backend/config/SecurityConfig.java
+++ src/main/java/com/onion/backend/config/SecurityConfig.java
@@ -1,30 +1,46 @@
 package com.onion.backend.config;
 
+import com.onion.backend.JWT.JwtAuthenticationFilter;
+import com.onion.backend.JWT.JwtUtil;
+import com.onion.backend.service.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+  @Autowired
+  private final UserDetailsServiceImpl userDetailsService;
+  @Autowired
+  private final JwtUtil jwtUtil;
+
+  public SecurityConfig(UserDetailsServiceImpl userDetailsService ,JwtUtil jwtUtil) {
+    this.userDetailsService = userDetailsService;
+    this.jwtUtil = jwtUtil;
+  }
+
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http
+        .csrf(csrf -> csrf.disable())
         .authorizeHttpRequests((requests) -> requests
-            .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/api/users/signUp","/api/users/login").permitAll()  // Swagger UI와 API 문서에 대한 접근 허용
+            .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/api/users/signUp", "/api/auth/login").permitAll()  // Swagger UI와 API 문서에 대한 접근 허용
             .anyRequest().authenticated()  // 나머지 요청은 인증된 사용자만 접근 가능
-        );
-//        .formLogin(Customizer.withDefaults());
-//        .logout((logout) -> logout
-//            .permitAll()
-//        );
-
+        )
+        .addFilterBefore(new JwtAuthenticationFilter(jwtUtil, userDetailsService), UsernamePasswordAuthenticationFilter.class)  // 필터 등록
+        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);  // 세션 없음
     return http.build();
   }
 
@@ -32,4 +48,14 @@ public class SecurityConfig {
   public PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
   }
+
+  @Bean
+  public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+    return http.getSharedObject(AuthenticationManagerBuilder.class)
+        .userDetailsService(userDetailsService)  // 사용자 인증 정보를 서비스에서 가져오기
+        .passwordEncoder(passwordEncoder())
+        .and()
+        .build();
+  }
+
 }
---
+++ src/main/java/com/onion/backend/config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package com.onion.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+
+@Configuration
+public class SwaggerConfig {
+
+  @Bean
+  public OpenAPI customOpenAPI() {
+    return new OpenAPI()
+        .info(new Info().title("Demo API").description("Demo API Documentation").version("1.0.0"))
+        .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+        .components(new io.swagger.v3.oas.models.Components()
+            .addSecuritySchemes("bearerAuth", new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+            ));
+  }
+}
