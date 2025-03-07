2bc89f23af59b4b2f8e98ddc3a1261885777fcce
Fillsogood <jdd05273@naver.com>
2025-03-07 11:47:37 +0900

    ForbiddenException 예외처리 기능

--- src/main/java/com/onion/backend/handler/GlobalExceptionHandler.java
+++ src/main/java/com/onion/backend/handler/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.onion.backend.handler;
 
+import com.onion.backend.exception.ForbiddenException;
 import com.onion.backend.exception.RateLimitException;
 import com.onion.backend.exception.ResourcNotFoundException;
 import org.springframework.http.HttpStatus;
@@ -9,15 +10,24 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+  //404Not Found
   @ExceptionHandler(ResourcNotFoundException.class)
   @ResponseStatus(HttpStatus.NOT_FOUND)
   public String handleResourceNotFoundException(ResourcNotFoundException e) {
     return e.getMessage();
   }
 
+  //5분 이상 예외 처리
   @ExceptionHandler(RateLimitException.class)
+  @ResponseStatus(HttpStatus.TOO_MANY_REQUESTS)
+  public String handleResourceLimitException(RateLimitException e) {
+    return e.getMessage();
+  }
+
+  //권한
+  @ExceptionHandler(ForbiddenException.class)
   @ResponseStatus(HttpStatus.FORBIDDEN)
-  public String handleResourceFORBIDDENException(RateLimitException e) {
+  public String handleResourceFORBIDDENException(ForbiddenException e) {
     return e.getMessage();
   }
 }
