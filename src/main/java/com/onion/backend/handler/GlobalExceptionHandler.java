76a687728953c17f75f75288a35f0e4f8efccbd1
Fillsogood <jdd05273@naver.com>
2025-03-07 09:37:38 +0900

    RateLimitException 예외처리 기능

--- src/main/java/com/onion/backend/handler/GlobalExceptionHandler.java
+++ src/main/java/com/onion/backend/handler/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.onion.backend.handler;
 
+import com.onion.backend.exception.RateLimitException;
 import com.onion.backend.exception.ResourcNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,7 +11,13 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
   @ExceptionHandler(ResourcNotFoundException.class)
   @ResponseStatus(HttpStatus.NOT_FOUND)
-  public String handleResourcNotFoundException(ResourcNotFoundException e) {
+  public String handleResourceNotFoundException(ResourcNotFoundException e) {
+    return e.getMessage();
+  }
+
+  @ExceptionHandler(RateLimitException.class)
+  @ResponseStatus(HttpStatus.FORBIDDEN)
+  public String handleResourceFORBIDDENException(RateLimitException e) {
     return e.getMessage();
   }
 }
